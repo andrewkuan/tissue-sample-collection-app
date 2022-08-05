fcf4cd0ccdad61da5450fc4ae20dbac625811380
Andrew Kuan <andrewkuan@Andrews-MacBook-Air.local>
2022-08-05 22:07:18 +0100

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,3 +21,26 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPuInNRsNxnM3glnVVuB0XgknyEXVfMDM",
+  authDomain: "tissue-sample-collection-app.firebaseapp.com",
+  projectId: "tissue-sample-collection-app",
+  storageBucket: "tissue-sample-collection-app.appspot.com",
+  messagingSenderId: "79317614287",
+  appId: "1:79317614287:web:e64b6683aec1efa7da81ba",
+  measurementId: "G-M27LR47Q3B"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
